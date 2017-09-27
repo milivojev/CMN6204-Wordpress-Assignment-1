b8cf5584608d63ee486cb00974a123c738b64f64
milivojev <vas.milivoj@gmail.com>
2017-09-27 22:06:27 +0200

    assigment 2

---
+++ wp-content/themes/Assigment1/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title><?php bloginfo('name'); ?>~<?php bloginfo('description'); ?></title>
+</head>
+<body>
+
+	<nav id="main-menu">
+		<ul>
+			<li><a href="#">Page1</a></li>
+			<li><a href="#">Page2</a></li>
+			<li><a href="#">Page3</a></li>
+			<li><a href="#">Page4</a></li>
+		</ul>
+	</nav>
